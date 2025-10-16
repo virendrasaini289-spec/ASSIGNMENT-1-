07506c251596aea7ae3ec874cb49d057f8569a5f
virendrasaini289-spec <virendrasaini289@gmail.com>
2025-10-16 09:34:07 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,9 @@
+a = int(input("Write your first number :-  "))
+b = int(input("Write your sceond number :-  "))
+
+print("all operations")
+
+print("addition :- ",a+b)
+print("Subtraction :- ",a-b)
+print("multiplication :- ",a*b)
+print("divison :- ",a/b)
